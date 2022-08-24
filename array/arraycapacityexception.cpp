874dd6dc8d6470dbdad3a23bcc7feb6572f0f9ad
Filipe Cruz <bad-alloc-error@yandex.com>
2022-08-24 11:35:39 -0300

    classe de exception

---
+++ array/arraycapacityexception.cpp
@@ -0,0 +1,11 @@
+#include "arraycapacityexception.hpp"
+
+ArrayCapacityException::ArrayCapacityException(std::string message){
+    error_message = message;
+}
+
+void ArrayCapacityException::log_message(){
+    std::cerr << error_message << std::endl;
+}
+
+
